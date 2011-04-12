44c8ed0104102c8bf21f25e1ab536d5fe1dca1c5
Damian Dobroczy\\'nski <qoocku@gmail.com>
2011-04-12 13:18:33 +0200

    final fix to mixins exclusions ;)

--- priv/examples/t3.erl
+++ priv/examples/t3.erl
@@ -1,5 +1,4 @@
 %% An example of explicit import with undefined exports in mixin module.
 -module (t3).
 -compile ([{parse_transform, mixins_pt}]).
--mixins ([{a, [a/1, unknown/3, a/3, a/4]},
-          b, {b2, {exclude, [f2/0]}}]).
+-mixins ([b, {b2, {exclude, [f2/0]}}]).
